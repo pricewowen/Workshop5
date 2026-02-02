220dfb722fadfdac7c304143161586b2ea87c3ea
masongga <auckmason@gmail.com>
2026-02-02 16:59:58 -0700

    added logError Method

--- src/main/java/com/sait/workshop05/logging/LogData.java
+++ src/main/java/com/sait/workshop05/logging/LogData.java
@@ -16,19 +16,31 @@ public class LogData {
      * @param log the log to be saved
      */
     public static void saveLog(Log log) {
-        // get current date and time
-        LocalDateTime currentTime = LocalDateTime.now();
-        String currentTimeString = (currentTime.getYear() + "-" + currentTime.getMonthValue() + "-"
-                + currentTime.getDayOfMonth() + "|" + currentTime.getHour() + ":" + currentTime.getMinute()
-                + ":" + currentTime.getSecond());
-
         if (log.getUser() != null && log.getAction() != null && log.getDescription() != null) {
             try (PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(fileName, true)))){
-                String line = ("USER="+ log.getUser() + " | " + log.getAction() + "=" + log.getDescription() + " | " + currentTimeString);
+                String line = ("USER="+ log.getUser() + " | " + log.getAction() + "=" + log.getDescription() + " | " + log.getCurrentDate());
                 out.append(line);
                 out.println();
             } catch (IOException e) {
-                throw new RuntimeException(e);
+                logError(new Log(log.getUser(), "error", e.getMessage()));
+            }
+        }
+    }
+
+    /**
+     * Logs errors in the Log.txt file
+     * @param log the log to be saved
+     */
+    public static void logError(Log log) {
+        if (log.getUser() != null && log.getAction() != null && log.getDescription() != null) {
+            try (PrintWriter out =  new PrintWriter(new BufferedWriter(new FileWriter(fileName, true)))) {
+                String line = ("USER=" + log.getUser() + " | " + log.getAction() + "_FAILED=" + log.getDescription() + " | " + log.getCurrentDate());
+                out.append(line);
+                out.println();
+            } catch (IOException e) {
+                System.err.println("ERROR: Could not write to log file");
+                System.err.println("USER=" + log.getUser() + " | " + log.getAction() + "_FAILED=" + log.getDescription() + " | " + log.getCurrentDate());
+                e.printStackTrace();
             }
         }
     }
--- src/main/java/com/sait/workshop05/models/Log.java
+++ src/main/java/com/sait/workshop05/models/Log.java
@@ -1,14 +1,31 @@
 package com.sait.workshop05.models;
 
+import java.time.LocalDateTime;
+
 public class Log {
     private  String user;
     private String action;
     private String description;
+    private String currentDate;
 
     public Log(String user, String action, String description) {
+        LocalDateTime currentTime = LocalDateTime.now();
+        String currentTimeString = (currentTime.getYear() + "-" + currentTime.getMonthValue() + "-"
+                + currentTime.getDayOfMonth() + "|" + currentTime.getHour() + ":" + currentTime.getMinute()
+                + ":" + currentTime.getSecond());
+
         this.user = user.toUpperCase();
         this.action = action.toUpperCase();
         this.description = description.toUpperCase();
+        this.currentDate = currentTimeString;
+    }
+
+    public String getCurrentDate() {
+        return currentDate;
+    }
+
+    public void setCurrentDate(String currentDate) {
+        this.currentDate = currentDate;
     }
 
     public String getUser() {
