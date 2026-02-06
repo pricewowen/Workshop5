447bd863e3ee4a544809bd6482e9d8cee1fecc5a
masongga <auckmason@gmail.com>
2026-02-06 15:36:11 -0700

    added provinces to combo box

--- src/main/java/com/sait/workshop05/BakeryLocationsController.java
+++ src/main/java/com/sait/workshop05/BakeryLocationsController.java
@@ -1,12 +1,17 @@
 package com.sait.workshop05;
 
+import com.sait.workshop05.models.Province;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.util.StringConverter;
 
 public class BakeryLocationsController {
 
@@ -26,7 +31,13 @@ public class BakeryLocationsController {
     private Button btnUpdate;
 
     @FXML
-    private TableColumn<?, ?> colAddressId;
+    private ComboBox<Province> cboAddressProvince;
+
+    @FXML
+    private TableColumn<?, ?> colBakeryAddress;
+
+    @FXML
+    private TableColumn<?, ?> colBakeryCity;
 
     @FXML
     private TableColumn<?, ?> colBakeryEmail;
@@ -41,13 +52,28 @@ public class BakeryLocationsController {
     private TableColumn<?, ?> colBakeryPhone;
 
     @FXML
-    private Label lblStatus;
+    private TableColumn<?, ?> colBakeryPostalCode;
+
+    @FXML
+    private TableColumn<?, ?> colBakeryProvince;
+
+    @FXML
+    private Label lblStatus1;
 
     @FXML
     private TableView<?> tblBakeryLocations;
 
     @FXML
-    private TextField txtAddressId;
+    private TextField txtAddressCity;
+
+    @FXML
+    private TextField txtAddressLine1;
+
+    @FXML
+    private TextField txtAddressLine2;
+
+    @FXML
+    private TextField txtAddressPostalCode;
 
     @FXML
     private TextField txtBakeryEmail;
@@ -87,6 +113,45 @@ public class BakeryLocationsController {
     @FXML
     void onUpdate(ActionEvent event) {
 
+    }
+
+    @FXML
+    void initialize() {
+        ObservableList<Province> provinces = FXCollections.observableArrayList(
+                new Province("AB", "Alberta"),
+                new Province("BC", "British Columbia"),
+                new Province("MB", "Manitoba"),
+                new Province("NB", "New Brunswick"),
+                new Province("NL", "Newfoundland and Labrador"),
+                new Province("NT", "Northwest Territories"),
+                new Province("NS", "Nova Scotia"),
+                new Province("NU", "Nunavut"),
+                new Province("ON", "Ontario"),
+                new Province("PE", "Prince Edward Island"),
+                new Province("QC", "Quebec"),
+                new Province("SK", "Saskatchewan"),
+                new Province("YT", "Yukon")
+        );
+        // store province objects in combo box
+        cboAddressProvince.setItems(provinces);
+        cboAddressProvince.setValue(provinces.getFirst());
+
+        // convert the province object into names
+        cboAddressProvince.setConverter(new StringConverter<Province>() {
+            @Override
+            public String toString(Province province) {
+                if (province != null) {
+                    return province.getName();
+                } else {
+                    return "";
+                }
+            }
+
+            @Override
+            public Province fromString(String s) {
+                return null;
+            }
+        });
     }
 
 }
