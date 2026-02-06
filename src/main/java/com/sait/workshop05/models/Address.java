c8ea5a77379ab7b3ece578ae7dbeec27716829c7
masongga <auckmason@gmail.com>
2026-02-06 14:49:45 -0700

    added address inputs to bakery locations and adjusted spacing

---
+++ src/main/java/com/sait/workshop05/models/Address.java
@@ -0,0 +1,10 @@
+package com.sait.workshop05.models;
+
+public class Address {
+    private int addressId;
+    private String addressLine1;
+    private String addressLine2;
+    private String addressCity;
+    private String addressProvince;
+    private String addressPostalCode;
+}
