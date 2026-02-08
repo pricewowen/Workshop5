3ca3c1d99c975261f7686d91a572219408399561
masongga <auckmason@gmail.com>
2026-02-07 18:32:11 -0700

    locations now display

--- src/main/java/com/sait/workshop05/database/BakeryDAO.java
+++ src/main/java/com/sait/workshop05/database/BakeryDAO.java
@@ -1,35 +1,52 @@
-//package com.sait.workshop05.database;
-//
-//import com.sait.workshop05.models.Address;
-//import com.sait.workshop05.models.Bakery;
-//
-//import java.sql.Connection;
-//import java.sql.PreparedStatement;
-//import java.sql.ResultSet;
-//import java.sql.SQLException;
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//public class BakeryDAO {
-//    public List<Bakery> getAllBakeries() throws SQLException {
-//        String sql = "SELECT b.bakeryId, b.bakeryName, b.bakeryPhone, b.bakeryEmail" +
-//                " a.addressLine1, a.addressLine2, a.addressCity, a.addressProvince" +
-//                " a.addressPostalCode" +
-//                " FROM bakery b JOIN Address a ON e.addressId = a.addressId";
-//
-//        ArrayList<Bakery> bakeries = new ArrayList<Bakery>();
-//
-////        try {
-////            Connection conn = DBUtil.getConnection();
-////            PreparedStatement stmt = conn.prepareStatement(sql);
-////            ResultSet rs = stmt.executeQuery();
-////
-////            while (rs.next()) {
-////                Address a = new Address();
-////                a.getAddressLine1(rs.getString("addressLine1")),
-////                a.getAddressLine2(rs.getString("addressLine2")),
-////
-////            }
-////        }
-//    }
-//}
+package com.sait.workshop05.database;
+
+import com.sait.workshop05.models.Address;
+import com.sait.workshop05.models.Bakery;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class BakeryDAO {
+    public ArrayList<Bakery> getAllBakeries() throws SQLException {
+        String sql = "SELECT b.bakeryId, b.bakeryName, b.bakeryPhone, b.bakeryEmail, " +
+                "a.addressLine1, a.addressLine2, a.addressCity, a.addressProvince, " +
+                "a.addressPostalCode, a.addressId " +
+                "FROM Bakery b JOIN Address a ON b.addressId = a.addressId";
+
+        ArrayList<Bakery> bakeries = new ArrayList<>();
+
+        try (Connection conn = DBUtil.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql);
+             ResultSet rs = ps.executeQuery()) {
+
+            while (rs.next()) {
+                // get address
+                Address a = new Address(
+                        rs.getInt("addressId"),
+                        rs.getString("addressLine1"),
+                        rs.getString("addressLine2"),
+                        rs.getString("addressCity"),
+                        rs.getString("addressProvince"),
+                        rs.getString("addressPostalCode")
+                );
+
+                // get bakery
+                Bakery b = new Bakery(
+                        rs.getInt("bakeryId"),
+                        a,
+                        rs.getString("bakeryName"),
+                        rs.getString("bakeryPhone"),
+                        rs.getString("bakeryEmail")
+                );
+
+                // add to bakery arrayList
+                bakeries.add(b);
+            }
+
+            return bakeries;
+        }
+    }
+}
