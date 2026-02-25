9204827b74b82fbda3bdac385b22a055384bfa80
masongga <auckmason@gmail.com>
2026-02-24 20:43:52 -0700

    Logs for employees only show employee logs

--- src/main/java/com/sait/workshop05/controllers/ActivityLogController.java
+++ src/main/java/com/sait/workshop05/controllers/ActivityLogController.java
@@ -2,6 +2,7 @@ package com.sait.workshop05.controllers;
 
 import com.sait.workshop05.logging.LogData;
 import com.sait.workshop05.models.Log;
+import com.sait.workshop05.session.UserSession;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
 
@@ -18,7 +19,63 @@ public class ActivityLogController {
 
     @FXML
     void initialize() {
-        showLogs();
+        UserSession session = UserSession.getInstance();
+        if(session.isAdmin()) {
+            showLogs();
+        } else if (session.isEmployee()){
+            showEmpLogs();
+        }
+    }
+
+    /**
+     * Displays logs according to logged-in user
+     */
+    public void showEmpLogs() {
+        UserSession session = UserSession.getInstance();
+        String username = session.getCurrentUser().getUsername();
+
+        ArrayList<String> listLogs = readEmpLogs(username);
+
+        // reverse logs so newest are on top
+        Collections.reverse(listLogs);
+
+        // display the logs in the ListView
+        for (String log : listLogs) {
+            lstLogs.getItems().add(log);
+        }
+    }
+
+    /**
+     * Reads the logs from the Log.txt file only for USER=username
+     * @param username of the USER to show logs for
+     * @return an ArrayList of Strings containing all the logs for the specific Employee
+     */
+    private static ArrayList<String> readEmpLogs(String username) {
+        // empty list
+        ArrayList<String> logs = new ArrayList<String>();
+
+        String line;
+        String[] fields;
+        Log tempLog;
+
+        // read the logs from the text file
+        try (BufferedReader in = new BufferedReader(new FileReader("Log.txt"))) {
+            line = in.readLine();
+
+            while (line != null) {
+
+                // sort file by username
+                if (line.contains("USER=" + username.toUpperCase())) {
+                    // add the line
+                    logs.add(line);
+                }
+                line = in.readLine();
+            }
+        } catch (IOException e) {
+            LogData.handleException("GET_LOGS", e);
+        }
+
+        return logs;
     }
 
     /**
