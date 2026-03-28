fa5d5a368dc521daeee987864022a96f479f86c9
grape <grapeberryfruit@gmail.com>
2026-03-28 13:28:23 -0600

    Add API client and DTOs for authentication, refactor login flow, and remove unused DAO logic

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,11 +2,13 @@ module com.sait.workshop05 {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires java.net.http;
     requires jbcrypt;
-
+    requires com.fasterxml.jackson.databind;
 
     opens com.sait.workshop05 to javafx.fxml;
     opens com.sait.workshop05.models to javafx.base;
+    opens com.sait.workshop05.api.dto to com.fasterxml.jackson.databind;
 
     exports com.sait.workshop05;
     exports com.sait.workshop05.models;
