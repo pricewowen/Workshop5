ebfd4165a8a515c4da81925e07c63007dd653aa7
grape <grapeberryfruit@gmail.com>
2026-04-03 21:14:29 -0600

    Remove XML declaration from misc.xml

--- src/main/java/com/sait/workshop05/api/dto/AuthResponseDto.java
+++ src/main/java/com/sait/workshop05/api/dto/AuthResponseDto.java
@@ -20,7 +20,6 @@ public class AuthResponseDto {
     private boolean photoApprovalPending;
     private String token;
     private String role;
-    private String userId;
 
     public AuthResponseDto() {}
 
@@ -72,6 +71,4 @@ public class AuthResponseDto {
     public String getRole() { return role; }
     public void setRole(String role) { this.role = role; }
 
-    public String getUserId() { return userId; }
-    public void setUserId(String userId) { this.userId = userId; }
 }
